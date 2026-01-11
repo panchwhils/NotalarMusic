f445ec0d3c67595beb92c1e8712e0d3ba0eca8c3
AnonymousX1025 <notanony@hotmail.com>
2026-01-11 17:24:05 +0530

    Done

--- anony/helpers/_inline.py
+++ anony/helpers/_inline.py
@@ -57,7 +57,7 @@ class Inline:
                 ]
             ]
         else:
-            cbs = ["admins", "auth", "blist", "lang", "ping", "play", "queue", "stats", "sudo"]
+            cbs = ["admins", "auth", "blist", "lang", "ping", "play", "queue", "stats", "sudo", "tagger", "fun"]
             buttons = [
                 self.ikb(text=_lang[f"help_{i}"], callback_data=f"help {cb}")
                 for i, cb in enumerate(cbs)
--- anony/plugins/tagger.py
+++ anony/plugins/tagger.py
@@ -7,12 +7,55 @@ import random
 import asyncio
 from pyrogram import enums, filters, types
 
-from anony import app, lang
+from anony import app, db, lang, queue
 from anony.helpers import admin_check
 
 
 running = []
 emojis = ["🤞", "💕", "😘", "🙌", "😎", "👌", "😊", "❤️", "😍"]
+
+
+@app.on_message(filters.command(["vctag"]) & filters.group & ~app.bl_users)
+@lang.language()
+@admin_check
+async def _vctag(_, m: types.Message):
+    if not await db.get_call(m.chat.id):
+        return await m.reply_text(m.lang["not_playing"])
+
+    if m.chat.id in running:
+        return await m.reply_text("Etiketleme işlemi zaten çalışıyor.")
+
+    await m.reply_text("Etiketleme işlemi başlatıldı.")
+    running.append(m.chat.id)
+
+    members = [
+        member async for member in app.get_chat_members(m.chat.id, limit=40)
+        if not member.user.is_bot
+    ]
+
+    media = queue.get_current(m.chat.id)
+    vc_text = f"<u><b>Neredesiniz ya? Video sohbete katılsanıza.</b></u>\n\n<b>Çalıyor: {media.title}</b>\n\n"
+    count, text = 0, ""
+    for member in members:
+        if m.chat.id not in running:
+            break
+
+        count += 1
+        text += member.user.mention + " "
+        if count == 5:
+            await app.send_message(
+                chat_id=m.chat.id,
+                text=vc_text + text,
+            )
+            count, text = 0, ""
+            await asyncio.sleep(3)
+
+    if count > 0:
+        await app.send_message(
+            chat_id=m.chat.id,
+            text=vc_text + text,
+        )
+    if m.chat.id in running: running.remove(m.chat.id)
 
 
 @app.on_message(filters.command(["utag", "atag", "etag", "bitir"]) & filters.group & ~app.bl_users)
